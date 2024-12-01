adab91ff59391604a72fa1ead31a2e653cc636e6
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2024-12-01 16:40:57 -0300

    add flutter in lsp

--- lua/user/plugins/lsp/mason.lua
+++ lua/user/plugins/lsp/mason.lua
@@ -1,31 +1,24 @@
 return {
-  "williamboman/mason.nvim",
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
+    "williamboman/mason.nvim", -- Ensure mason is installed
     "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    { "antosha417/nvim-lsp-file-operations", config = true },
+    { "folke/neodev.nvim", opts = {} },
   },
   config = function()
-    -- import mason
     local mason = require("mason")
-
-    -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
+    local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-    local mason_tool_installer = require("mason-tool-installer")
+    -- Enable mason
+    mason.setup()
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
-
+    -- Setup mason-lspconfig
     mason_lspconfig.setup({
-      -- list of servers for mason to install
       ensure_installed = {
         "tsserver",
         "html",
@@ -35,21 +28,68 @@ return {
         "lua_ls",
         "graphql",
         "emmet_ls",
-        "prismals",
-        "pyright",
         "clangd",
+        "pyright",
+        -- Note: dartls is handled manually for Flutter
       },
     })
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
-        "eslint_d",
+    -- Capabilities for autocompletion
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+
+    -- Change diagnostic signs
+    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
+
+    -- Use mason-lspconfig's setup handlers for all installed servers
+    mason_lspconfig.setup_handlers({
+      function(server_name)
+        lspconfig[server_name].setup({
+          capabilities = capabilities,
+        })
+      end,
+      ["lua_ls"] = function()
+        lspconfig.lua_ls.setup({
+          capabilities = capabilities,
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+          },
+        })
+      end,
+    })
+
+    -- Manual setup for dartls (Flutter support)
+    lspconfig.dartls.setup({
+      cmd = { "dart", "language-server", "--protocol=lsp" },
+      filetypes = { "dart" },
+      init_options = {
+        closingLabels = true,
+        flutterOutline = true,
+        onlyAnalyzeProjectsWithOpenFiles = true,
+        outline = true,
+        suggestFromUnimportedLibraries = true,
       },
+      settings = {
+        dart = {
+          completeFunctionCalls = true,
+          showTodos = true,
+        },
+      },
+      on_attach = function(client, bufnr)
+        -- Flutter LSP setup, for example key mappings
+        -- You can customize the key mappings as per your preference
+      end,
     })
   end,
 }
+
